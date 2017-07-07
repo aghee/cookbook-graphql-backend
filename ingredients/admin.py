dfc713b2c377d3e454cd83fc71ec13bd52920e5b
Humphrey Mugambi <mugagambi@gmail.com>
2017-07-07 11:01:44 +0300

    created models, added data and created admin

--- ingredients/admin.py
+++ ingredients/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from ingredients import models
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'notes', 'category')
+
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Ingredient, IngredientAdmin)
+
+admin.site.index_title = 'CookBook'
+admin.site.site_title = 'CookBook'
+admin.site.site_header = 'CookBook'
